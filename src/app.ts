9486f4b734c9fe18f8cc2e8ba5cd37ffbb76cf27
RhuanMr <magicknight@hotmail.com.br>
2025-05-04 14:53:10 -0300

    Created te app file

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+// import { conversationRouter } from './routes/conversationRoutes';
+// import { userRouter } from './routes/userRoutes';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// app.use('/api/conversations', conversationRouter);
+// app.use('/api/users', userRouter);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
