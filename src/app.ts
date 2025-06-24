c7b929b3640ef994e7bf7bfaf16feb00b3593442
RhuanMr <magicknight@hotmail.com.br>
2025-06-24 17:39:30 -0300

    Implemante routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-// import { conversationRouter } from './routes/conversationRoutes';
-// import { userRouter } from './routes/userRoutes';
+import { conversationRouter } from './routes/ConversationRoutes';
+import { userRouter } from './routes/UserRoutes';
 
 dotenv.config();
 
@@ -12,8 +12,8 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-// app.use('/api/conversations', conversationRouter);
-// app.use('/api/users', userRouter);
+app.use('/api/conversations', conversationRouter);
+app.use('/api/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
